fd721a902b703de31ce525e94e32b1cf3a5ddca0
Žan Sovič <soviczan7@gmail.com>
2024-05-03 16:27:48 +0200

    new test

--- test.c
+++ test.c
@@ -5,12 +5,16 @@
 #include "generic_lexer.h"
 
 int main(void) {
-    Lexer lexer = lexer_init("main.c", "int main(void) { return 0; }");
+    Lexer lexer = lexer_init("main.c", "int main(void) { bool success = true; if (success) return 0;\nelse return 1; }");
     Tokens tokens = lexer_lex(&lexer);
 
     for (size_t i = 0; i < tokens.size; i++) {
         printf("%s\n", tokens.items[i].value.items);
     }
 
+    for (size_t i = 0; i < tokens.size; i++) {
+        free(tokens.items[i].value.items);
+    }
+    free(tokens.items);
     return 0;
 }
